12d83374492a85502261f0e01ee7353b2397537d
SansReves <ig.mendezs@duocuc.cl>
2023-07-11 14:45:52 -0400

    form_serv

--- LibreriaImaginaV2/core/views.py
+++ LibreriaImaginaV2/core/views.py
@@ -6,10 +6,11 @@ from django.contrib import messages
 import re
 
 from .forms import CustomUserCreationForm
-from .models import Libro, Cliente,TipoCliente, Servicio, OrdenServicio
+from .models import Libro, Cliente,TipoCliente, Servicio, OrdenServicio,OrdenCompra, TipoPago
 
 import cx_Oracle
 from django.http import HttpResponse
+from datetime import date,datetime
 
 def home(request):
     return render(request,'core/home.html')
@@ -26,6 +27,10 @@ def iniciosesion(request):
             # Almacena el nombre del cliente en una cookie
             response = redirect('home')
             response.set_cookie('cliente_nombre', cliente.nombre_cli)
+            response.set_cookie('cliente_rut', cliente.rut_cli)
+            response.set_cookie('cliente_correo', cliente.correo_cli)
+            response.set_cookie('cliente_telefono', str(cliente.telefono_cli))
+
             return response
         except Cliente.DoesNotExist:
             # Usuario o contraseña incorrectos
@@ -75,9 +80,6 @@ def registrouser(request):
         # Eliminar guiones y puntos del rut_cli
         rut_cli = re.sub(r'[-\.]', '', rut_cli)
 
-        # Verificar si las contraseñas coinciden
-        if contrasenia_cli != confirm_contrasenia_cli:
-            return render(request, 'registration/registrouser.html', {'error': 'Las contraseñas no coinciden'})
         # Validar que el rut_cli tenga un largo admitido de 9 u 8 caracteres
         if len(rut_cli) not in [8, 9]:
             return render(request, 'registration/registrouser.html', {'error': 'El RUT debe tener 8 o 9 caracteres.'})
@@ -88,6 +90,11 @@ def registrouser(request):
         # Validar que el último carácter sea "K" o un dígito
         if not rut_cli[-1].isdigit() and rut_cli[-1] != 'K':
             return render(request, 'registration/registrouser.html', {'error': 'El último carácter debe ser "K" o un dígito.'})
+        
+        # Verificar si las contraseñas coinciden
+        if contrasenia_cli != confirm_contrasenia_cli:
+            return render(request, 'registration/registrouser.html', {'error': 'Las contraseñas no coinciden'})
+        
         # Obtener la instancia de TipoCliente correspondiente al id_tp_cli
         tipo_cliente = TipoCliente.objects.get(id_tp_cli=id_tp_cli)
 
@@ -111,28 +118,75 @@ def registrouser(request):
     return render(request, 'registration/registrouser.html', {'tipos_clientes': tipos_clientes})
 
 def form_servicio(request):
-    #Combo box
-    list_serv = Servicio.objects.all
-
-    #Solicitud de Servicio
     if request.method == 'POST':
-        orp = OrdenServicio()
+        id_serv = request.POST.get("id_serv")
+        request.session['id_serv'] = id_serv
+        cliente_rut = request.COOKIES.get('cliente_rut')
+        fecha_actual = date.today()
+        # Obtener los datos de la solicitud POST
+        fecha_or_compra = date.today()
+        total_final_or = request.POST.get('total_final_or')
+        rut_emp = '12345678K' #se usa para las compras hechas por la pagina web
+        id_est = 1 #estado en proceso
+        id_tp_pago = 2
+        rut_cli = cliente_rut
         
-        orp.id_serv = request.POST.get("")
-        orp.fecha_serv = request.POST.get("fecha")
-        orp.cant_serv = 1
-        orp.total_detalle_serv = request.POST.get("")
-        orp.detalle_serv = request.POST.get("detalle")
-
-        serv = Servicio.objects.all()
-        serv.id_serv = request.POST.get("cboServicio")
-        orp.id_serv = serv
-
+        
+        tipo_pago = TipoPago.objects.get(id_tp_pago=id_tp_pago)
+        # Establecer la conexión a la base de datos Oracle
+        conn = cx_Oracle.connect('bdLibreriav2/bdLibreriav2@127.0.0.1:1521/xe')
+        
         try:
-            orp.save()
-            mensaje = "Guardado correctamente"
-            messages.success(request, mensaje)
-        except:
-            mensaje = "Error al Enviar"
-            messages.error(request, mensaje)
-    return render(request,'formularios/form_servicio.html')
+            # Crear un cursor
+            cursor = conn.cursor()
+            
+            # Llamar al procedimiento almacenado 'save_or_compra' y obtener el ID de compra generado
+            id_compra = cursor.var(cx_Oracle.NUMBER)
+            cursor.callproc(
+                'save_or_compra',
+                [fecha_or_compra, total_final_or, rut_emp, id_est, id_tp_pago, rut_cli, id_compra]
+            )
+
+            # Obtener el valor del ID de compra generado
+            id_compra_generado = id_compra.getvalue()
+            tipo_servicio = Servicio.objects.get(id_serv=id_serv)
+            
+
+            # Obtener los datos de la solicitud POST para orden_servicio
+            total_detalle_serv = request.POST.get('total_detalle_serv')
+            cant_serv = request.POST.get('cant_serv')
+
+            fecha_serv = request.POST.get('fecha_serv')
+            fecha_serv = datetime.strptime(fecha_serv, '%Y-%m-%d').date()  # Convertir la fecha a objeto date
+            if fecha_serv < fecha_actual:
+                return HttpResponse('La fecha de servicio no puede ser anterior a la fecha actual.')
+            
+            detalle_serv = request.POST.get('detalle_serv')
+            confirm_serv = request.POST.get('confirm_serv')
+            id_serv = request.POST.get('id_serv')
+
+            # Llamar al procedimiento almacenado 'save_or_servicio' para insertar en la tabla 'orden_servicio'
+            cursor.callproc(
+                'save_or_servicio',
+                [total_detalle_serv, cant_serv, fecha_serv, detalle_serv, confirm_serv, tipo_servicio, id_compra_generado]
+            )
+
+            # Confirmar la transacción
+            conn.commit()
+            # Cerrar el cursor
+            cursor.close()
+            # Cerrar la conexión
+            conn.close()
+
+            # Retorna una respuesta exitosa
+            return HttpResponse('La orden de compra y la orden de servicio se han guardado exitosamente.')
+
+        except cx_Oracle.Error as error:
+            # Manejar cualquier error de Oracle
+            print('Error de Oracle:', error)
+            # Opcionalmente, puedes agregar un mensaje de error personalizado en la respuesta HTTP
+            return HttpResponse('Ocurrió un error al guardar la orden de compra y la orden de servicio.')
+        
+    tipos_pagos = TipoPago.objects.all()
+    tipos_servicios = Servicio.objects.all()
+    return render(request, 'formularios/form_servicio.html', {'tipos_servicios': tipos_servicios,'tipos_pagos': tipos_pagos, 'id_serv': id_serv})
